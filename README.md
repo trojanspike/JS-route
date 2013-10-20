73e257f2e190cb6a1e6e5d475df95c82ee3e8d4e
Lee <trojan_spikepk@yahoo.co.uk>
2013-10-20 09:37:42 -0400

    some files

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ Quick JS router library which queries $_GET and HASH
 
 ## usage
 ``` js
-    Router('get')
+    Router('get').isset('key' , function(value){
+        // will only run callback when $_GET->key param is found
+        // callback param value = ?key=value
+    })
+    
+    // multi array keys set
+    Rouer('get').isset(['key1' , 'key2', 'key3'], function(val1, val2, val3){
+        // again will only run the callback when all $_GET params are found
+        // callback params val{1,2.3} = ?key1=val1&key2=val2&key3=val3
+    });
     
     Router('hash')
--- src/router.js
+++ src/router.js
@@ -1,6 +1,38 @@
 (function(win, doc, undefined){
-/**/    
-    
+/*=========*/
+var _routeGet,
+_url = win.location.href;
+
+_routeGet = function(){
+    var _getRegX = new RegExp(/^.*(\?[A-Za-z0-9_=&%]+)(.*|#)$/);
+    if(_getRegX.test(_url)){
+        this.get = _url.replace(_getRegX, '$1');
+    } else {
+        this.get = false;
+    }
+    return this;
+};
+_routeGet.prototype = {
+    isset : function(getQ, callback){
+        var _rg = new RegExp('^.*'+getQ+'=([A-Za-z0-9%@]+).*$');
+        if(_rg.test(this.get) && typeof getQ === 'string'){
+            callback(this.get.replace(_rg, '$1').replace(/%20/g, ' '));
+        }
+    }
+};
+/*==========================================*/
+/* attach - window | requireJS | window.SIG */
+window.Router = function(which){
+    switch(which.toUpperCase()){
+        case 'GET':
+            return new _routeGet();
+        break;
+        /**/
+        case 'HASH':
+            
+        break;
+    }
+};
 
 /**/
 })(window, document);
